64a17b93732911c53feb84b27740d46602b4c6f7
CronosSenpai <jordivs00@gmail.com>
2020-03-23 22:23:45 +0100

    initial Commit

---
+++ Base.py
@@ -0,0 +1,60 @@
+import discord
+import random
+import youtube_dl
+from discord.ext import commands
+from discord.utils import get
+import os
+
+#Prefijo de llamada
+client = commands.Bot(command_prefix= '.')
+
+players = {}
+
+#Status 
+@client.event
+async def on_ready():
+    print("Bot is OK.")
+
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} entro akka we')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} se fue we')
+
+@client.command()
+async def comunismo(ctx): #ctx hace que lo adicional sea ignorado
+    await ctx.send(f'Fuera de aqui maldito comunista, aqui solo existen cerdos capitalistas {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = ['0','3','2','54', 'tetet', 'trpiac']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}') #\n pone lo siguiente en la proxima linea
+
+@client.command(pass_context=True)
+async def join(ctx): #entra en el server
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+
+@client.command(pass_context=True) #sale del server
+async def leave(ctx):
+    guild = ctx.message.guild
+    voice_client = guild.voice_client(guild)
+    await voice_client.disconnect()
+
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    guild = ctx.message.guild
+    voice_client = guild.voice_client(guild)
+    player = await voice_client.create_ytdl_player(url)
+    players[guild.id] = player
+    player.start()
+
+
+
+TOKEN= 'NjkxMjYyNDYyMDQwOTk3ODk4.XndaXg.g8y1R8EZZPlVEj9SCbQ6ytlYavg'
+#token
+client.run(TOKEN)
---
+++ Base2.py
@@ -0,0 +1,94 @@
+import discord 
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+import os
+
+TOKEN= 'NjkxMjYyNDYyMDQwOTk3ODk4.XndaXg.g8y1R8EZZPlVEj9SCbQ6ytlYavg' 
+BOT_PREFIX = '.'
+
+
+bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    print('ok')
+
+
+@bot.command(pass_context=True, aliases=['j', 'joi'])
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+    
+    await ctx.send(f"Joined {channel}")
+
+
+
+@bot.command(pass_context = True, aliases=['l' , 'lea'])
+async def leave(ctx):
+    voice = get(bot.voice_clients, guild= ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {voice.channel}")
+        await ctx.send(f" The bot has left {voice.channel}")
+
+    else:
+        print("Bot se fue del canal")
+        await ctx.send("Don't think i am in a voice channel")
+
+@bot.command(pass_context=True, alisases=['p', 'pla'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everython ready now")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format' : 'bestaudio/best',
+        'postprocessores': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print('Dowloading audio now\n')
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".webm"):
+            name = file
+            print(f"Renamed file : {file}\n")
+            os.rename(file, "song.mp3")
+
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname}")
+    print("playing\n")
+
+.j Trove
+
+bot.run(TOKEN)
